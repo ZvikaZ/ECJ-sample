124008a98dcfca0c6d3378364f261879409a7409
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-05-24 12:44:51 +0300

    ... and fixed run.*

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @rem TODO: relative paths
 
-java -Dfile.encoding=UTF-8 -classpath target/classes;local-maven-repo/edu/gmu/cs/ecj/27/ecj-27.jar SampleEvolve
+java -Dfile.encoding=UTF-8 -classpath target/classes;local-maven-repo/edu/gmu/cs/ecj/27.1-zvika-2/ecj-27.1-zvika-2.jar SampleEvolve
 
 
--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 #SBATCH --cpus-per-task=8 # 8 cpus per task - use for multithreading, usually with --tasks=1
 #SBATCH --tasks=1 # 1 processes - use for multiprocessing
 
-time $JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -classpath target/classes:local-maven-repo/edu/gmu/cs/ecj/27/ecj-27.jar SampleEvolve
+time $JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -classpath target/classes:local-maven-repo/edu/gmu/cs/ecj/27.1-zvika-2/ecj-27.1-zvika-2.jar SampleEvolve
 
