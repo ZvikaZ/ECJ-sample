cf4b27fe4679b9edbf14530328d22d544f990260
Zvika Haramaty <haramaty.zvika@gmail.com>
2021-08-10 10:59:13 +0300

    fix run.* to use new version

--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
-@rem TODO: relative paths
-
-java -Dfile.encoding=UTF-8 -classpath target/classes;local-maven-repo/edu/gmu/cs/ecj/27.1-zvika-2/ecj-27.1-zvika-2.jar SampleEvolve
+java -Dfile.encoding=UTF-8 -classpath target/classes;%userprofile%\.m2\repository\com\github\ZvikaZ\ecj\27.1-zvika-3\ecj-27.1-zvika-3.jar SampleEvolve
 
 
--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 #SBATCH --cpus-per-task=8 # 8 cpus per task - use for multithreading, usually with --tasks=1
 #SBATCH --tasks=1 # 1 processes - use for multiprocessing
 
-time $JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -classpath target/classes:local-maven-repo/edu/gmu/cs/ecj/27.1-zvika-2/ecj-27.1-zvika-2.jar SampleEvolve
+time $JAVA_HOME/bin/java -Dfile.encoding=UTF-8 -classpath target/classes:$HOME/.m2/repository/com/github/ZvikaZ/ecj/27.1-zvika-3/ecj-27.1-zvika-3.jar:/home/zvikah/.m2/repository/org/ejml/all/0.27/all-0.27.jar SampleEvolve
 
